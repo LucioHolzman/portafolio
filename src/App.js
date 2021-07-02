5d79ec99c0f733edda83df063fc8b3ef029be94a
Lucio Holzman <holzmanluciodamian@gmail.com>
2021-07-01 22:49:17 -0300

    redesign

--- src/App.js
+++ src/App.js
@@ -3,10 +3,13 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-        En este momento la página está siendo rediseñada.
-        </p>
+      <header className="header">
+        <div className="container">
+          <div className="containerText"> 
+            <h1>En este momento la página está siendo rediseñada.</h1>
+            <p>Lucio Holzman</p>
+          </div>
+        </div>
       </header>
     </div>
   );
